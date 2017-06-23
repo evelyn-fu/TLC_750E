2de943fe4544f2898788326fc1afa20fcbdf3f9a
evelyn-fu <10013355@sbstudents.org>
2017-06-22 20:37:02 -0400

    Add files via upload

--- TLC_750E/include/variables.h
+++ TLC_750E/include/variables.h
@@ -11,21 +11,18 @@
 #define MOTOR_LIFT_RIGHT_ 7
 #define MOTOR_PINCE_LEFT_ 8
 #define MOTOR_PINCE_RIGHT_ 9
-#define ENCODER_LEFT1_TOP_ 1
-#define ENCODER_RIGHT1_TOP_ 2
-#define ENCODER_LEFT1_BOT_ 3
-#define ENCODER_RIGHT1_BOT_ 4
-#define ENCODER_LEFT2_TOP_ 5
-#define ENCODER_RIGHT2_TOP_ 6
-#define ENCODER_LEFT2_BOT_ 7
-#define ENCODER_RIGHT2_BOT_ 8
+#define ENCODER_LEFT_TOP_ 1
+#define ENCODER_RIGHT_TOP_ 2
+#define ENCODER_LEFT_BOT_ 3
+#define ENCODER_RIGHT_BOT_ 4
 
-Encoder encoderLeft1;
-Encoder encoderRight1;
-Encoder encoderLeft2;
-Encoder encoderRight2;
+Encoder encoderLeft;
+Encoder encoderRight;
 
 void drive(int ch1, int ch2, int ch3, int ch4);
 void lift(int speed);
 void pince(int speed);
+void pointTurnAuton(Encoder encoder1, Encoder encoder2, int distance);
+void driveForwardAuton(Encoder encoder1, Encoder encoder2, int degrees);
+
 #endif
--- TLC_750E/src/variables.c
+++ TLC_750E/src/variables.c
@@ -1,5 +1,6 @@
 #include "variables.h"
 #include "main.h"
+#include <math.h>
 
 void drive(int ch1, int ch2, int ch3, int ch4){//a corresponds to the individual analog button axes
   motorSet(MOTOR_BASE_LEFT1_, ch3 + ch2 + ch4 + ch1);
@@ -15,14 +16,37 @@ void pince(int speed) {
   motorSet(MOTOR_PINCE_RIGHT_, -speed);
   motorSet(MOTOR_PINCE_RIGHT_, speed);
 }
-void driveForwardAuton(Encoder encoder1, Encoder encoder2, int distance) {
+void driveForwardAuton(Encoder encoder1, Encoder encoder2, int distance) { //distance is in inches
+  int goal = distance * 28.66;
   int encoder1start = encoderGet(encoder1);
   int encoder2start = encoderGet(encoder2);
+  int encoder1count;
+  int encoder2count;
   while(1) {
-    int encoder1count = encoderGet(encoder1) - encoder1start;
-    motorSet(MOTOR_BASE_LEFT1_, 127);
-    motorSet(MOTOR_BASE_LEFT2_, 127);
-    motorSet(MOTOR_BASE_RIGHT1_, -127);
-    motorSet(MOTOR_BASE_RIGHT2_, -127);
+    encoder1count = (encoderGet(encoder1) / sqrt(2)) - encoder1start;
+    encoder2count = (encoderGet(encoder2) / sqrt(2)) - encoder2start;
+    if ((encoder1count < goal) && (encoder2count < goal)) {
+      motorSet(MOTOR_BASE_LEFT1_, 127);
+      motorSet(MOTOR_BASE_LEFT2_, 127);
+      motorSet(MOTOR_BASE_RIGHT1_, -127);
+      motorSet(MOTOR_BASE_RIGHT2_, -127);
+    }
+  }
+}
+void pointTurnAuton(Encoder encoder1, Encoder encoder2, int degrees) {
+  int goal = degrees/226.5;
+  int encoder1start = encoderGet(encoder1);
+  int encoder2start = encoderGet(encoder2);
+  int encoder1count;
+  int encoder2count;
+  while(1) {
+    encoder1count = encoderGet(encoder1) - encoder1start;
+    encoder2count = encoderGet(encoder2) - encoder2start;
+    if ((encoder1count < goal) && (encoder2count < goal)) {
+      motorSet(MOTOR_BASE_LEFT1_, 127);
+      motorSet(MOTOR_BASE_LEFT2_, 127);
+      motorSet(MOTOR_BASE_RIGHT1_, 127);
+      motorSet(MOTOR_BASE_RIGHT2_, 127);
+    }
   }
 }
